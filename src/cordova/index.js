401a70b391deb9766a75f31a35805188f3538cc0
CS-Tao <990296951@qq.com>
2018-11-09 00:21:09 +0800

    Reconstruct project by cordova-vue
    Add cordova plugins: cordova-plugin-advanced-http / cordova-plugin-advanced-http

---
+++ src/cordova/index.js
@@ -0,0 +1,6 @@
+var cordova = window.cordova
+
+// 后台模式
+cordova.plugins.backgroundMode.setEnabled(true)
+
+export default cordova
--- src/main.js
+++ src/main.js
@@ -6,7 +6,14 @@ import 'element-ui/lib/theme-chalk/index.css'
 
 import packageInfo from '../package.json'
 
-// import './api/mock'
+import './api/mock'
+
+if (window.location.protocol === 'file:' || window.location.port === '3000') {
+  var cordovaScript = document.createElement('script')
+  cordovaScript.setAttribute('type', 'text/javascript')
+  cordovaScript.setAttribute('src', 'cordova.js')
+  document.body.appendChild(cordovaScript)
+}
 
 Vue.use(ElementUI, { size: 'small', zIndex: 3000 })
 
@@ -14,7 +21,6 @@ Vue.config.productionTip = false
 
 Vue.http = Vue.prototype.$http = axios
 Vue.appVersion = Vue.prototype.$appVersion = packageInfo.version
-Vue.cordova = Vue.prototype.$cordova = window.cordova
 Vue.openLink = Vue.prototype.$openLink = (url) => { window.open(url) }
 Vue.exitApp = Vue.prototype.$exitApp = () => { navigator.app.exitApp() }
 
@@ -38,6 +44,7 @@ document.addEventListener('backbutton', () => {
 
 // 加载 Vue 实例
 document.addEventListener('deviceready', function () {
+  Vue.cordova = Vue.prototype.$cordova = require('./cordova').default
   new Vue({
     components: { App: require('./App').default },
     router: require('./router').default,
